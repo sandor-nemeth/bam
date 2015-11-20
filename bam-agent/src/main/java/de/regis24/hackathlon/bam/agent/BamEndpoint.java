b955e597fc84f8ad58523fdf88dfd280eed22b16
Sandor Nemeth <sandor.nemeth.1986@gmail.com>
2015-11-20 22:06:01 +0100

    endpoint now serves data, and also added host and port for registrationˆ

--- bam-agent/src/main/java/de/regis24/hackathlon/bam/agent/BamEndpoint.java
+++ bam-agent/src/main/java/de/regis24/hackathlon/bam/agent/BamEndpoint.java
@@ -1,8 +1,12 @@
 package de.regis24.hackathlon.bam.agent;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -20,7 +23,11 @@ public class BamEndpoint {
 
   @Autowired(required = false)
   private List<Job> jobs = new ArrayList<>();
-  @Autowired private BamJdbcDao bamJdbcDao;
+  @Autowired
+  private BamJdbcDao bamJdbcDao;
+  @Autowired
+  private JobExplorer jobExplorer;
+
 
   @RequestMapping("jobs")
   public List<JobDescription> jobs() {
@@ -38,11 +45,60 @@ public class BamEndpoint {
         .build();
   }
 
-  @Getter @Builder
+  @RequestMapping("jobs")
+  public List<JobStats> jobStats() {
+    return jobs.stream()
+        .map(this::lastJobExecution)
+        .filter(jobExecution -> jobExecution != null)
+        .map(this::toJobStats)
+        .collect(Collectors.toList());
+  }
+
+  private JobStats toJobStats(JobExecution jobExecution) {
+    int skippedItems = 0;
+    int writtenItems = 0;
+    for (StepExecution stepExecution : jobExecution.getStepExecutions()) {
+      skippedItems += stepExecution.getReadSkipCount()
+                      + stepExecution.getProcessSkipCount()
+                      + stepExecution.getWriteSkipCount();
+      writtenItems += stepExecution.getWriteCount();
+    }
+
+    return JobStats.builder()
+        .jobName(jobExecution.getJobInstance().getJobName())
+        .numberOfSkippedItems(skippedItems)
+        .numberOfWrittenItems(writtenItems)
+        .resultOfLastExecution(jobExecution.getExitStatus().getExitCode())
+        .build();
+  }
+
+  private JobExecution lastJobExecution(Job job) {
+    List<JobInstance> jobInstances = jobExplorer.getJobInstances(job.getName(), 0, 1);
+    if (!CollectionUtils.isEmpty(jobInstances)) {
+      List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstances.get(0));
+      if (!CollectionUtils.isEmpty(jobExecutions)) {
+        return jobExecutions.get(0);
+      }
+    }
+    return null;
+  }
+
+  @Getter
+  @Builder
   public static class Stats {
+
     private int numberOfExecutions;
     private int numberOfProcessedItems;
     private long totalExecutedTime;
   }
 
+  @Getter
+  @Builder
+  public static class JobStats {
+
+    String jobName;
+    int numberOfWrittenItems;
+    int numberOfSkippedItems;
+    String resultOfLastExecution;
+  }
 }
